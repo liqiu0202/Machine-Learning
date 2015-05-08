2bfd56cbb4f33f6645f61969d587a1c1f4092ed7
liqiu <qiuli90@gmail.com>
2015-05-07 19:04:15 -0700

    ex6 svm implementation

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -22,10 +22,31 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
+%
+% Below is the code to get optimal C and sigma, after we get their values, we comment these code for accelerating computation
+%C_candidate = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+%sigma_candidate = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+%error = 1e100;
+%for i = 1:length(C_candidate)
+%  for j = 1:length(sigma_candidate)
+%     %x1, x2 are just placeholder vectors and will be substitute later
+%     model= svmTrain(X, y, C_candidate(i), @(x1, x2) gaussianKernel(x1, x2, sigma_candidate(j) ));
+%     predictions = svmPredict(model, Xval);
+%     tmpError = mean(double(predictions ~= yval) );
+%     if tmpError < error
+%        C = C_candidate(i);
+%        sigma = sigma_candidate(j);
+%        error = tmpError;
+%     endif;
+%     printf("%d %d is completed", i, j);
+%  endfor;
+%endfor;
 
 
+%printf("c: %f sigma:%f\n", C, sigma);
 
 
+C = 1;  sigma = 0.1;
 
 
 
